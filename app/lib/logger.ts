bde817d21f9dfb867aa2e2b03b64d843a05d895f
David Mynors <hello@davidmyno.rs>
2023-11-29 07:57:42 +0000

    Simplify logger

--- app/lib/logger.ts
+++ app/lib/logger.ts
@@ -1,25 +1,7 @@
-import { pipe, Cause, Effect, Console } from "effect";
+import { Cause, Effect, Console } from "effect";
 
 export const logErrors = (p: Effect.Effect<never, unknown, unknown>) => {
-  return pipe(
-    p,
-    Effect.sandbox,
-    Effect.catchTags({
-      Die: (cause) =>
-        Console.log(`Caught a defect: ${cause.defect}`).pipe(
-          Effect.as("fallback result on defect")
-        ),
-      Interrupt: (cause) =>
-        Console.log(`Caught a defect: ${cause.fiberId}`).pipe(
-          Effect.as("fallback result on fiber interruption")
-        ),
-      Fail: (cause) =>
-        Console.log(`Caught a defect: ${cause.error}`).pipe(
-          Effect.as("fallback result on failure")
-        ),
-    }),
-    Effect.unsandbox
-  );
+  return logAndMapErrors(() => Effect.succeedNone)(p);
 };
 
 export const logAndMapErrors = <V, E>(
@@ -28,23 +10,9 @@ export const logAndMapErrors = <V, E>(
   p: Effect.Effect<never, unknown, V>
 ) => Effect.Effect<never, unknown, V | E>) => {
   return (p) =>
-    pipe(
-      p,
-      Effect.sandbox,
-      Effect.catchTags({
-        Die: (cause) =>
-          Console.log(`Caught a defect: ${cause.defect}`).pipe(
-            Effect.map(() => mapFn(cause))
-          ),
-        Interrupt: (cause) =>
-          Console.log(`Caught a defect: ${cause.fiberId}`).pipe(
-            Effect.map(() => mapFn(cause))
-          ),
-        Fail: (cause) =>
-          Console.log(`Caught a defect: ${cause.error}`).pipe(
-            Effect.map(() => mapFn(cause))
-          ),
-      }),
-      Effect.unsandbox
+    p.pipe(
+      Effect.catchAllCause((cause) =>
+        Console.log(cause).pipe(Effect.map(() => mapFn(cause)))
+      )
     );
 };
